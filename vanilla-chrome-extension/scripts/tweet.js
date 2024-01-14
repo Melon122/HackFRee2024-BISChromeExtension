0ce97f786609fcb3fb9bf5b851ced99b662bfc96
Melon122 <melvinpfranklin@gmail.com>
2024-01-13 23:03:06 -0500

    vanilla js restart

---
+++ vanilla-chrome-extension/scripts/tweet.js
@@ -0,0 +1,8 @@
+let tweetElement = document.querySelector('div[data-testid]="tweetText"]');
+
+const tweetSpan = tweetElement.querySelector("span")
+
+let tweetText = tweetSpan.textContent.trim()
+
+
+
